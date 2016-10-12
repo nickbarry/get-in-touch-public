ef0b43b295027275a10d8115a00b00da977c643d
Nick Barry <niko.barry@gmail.com>
2016-10-12 13:41:24 -0700

    feat: [WIP] Update state when there's a pending fetch of contacts
    Right now, there's no piece of redux state that records whether there's a pending fetch request for contacts (e.g., when the user loads the homepage container). This commit doesn't change that, but it does lay a teeny bit of groundwork for getting there - it creates an incomplete reducer file for the homepage. We'll need to finish that file, and update the store so it listens to that reducer to update the "fetchInProgress" (or something) state property.

--- app/containers/HomePage/actions.js
+++ app/containers/HomePage/actions.js
@@ -1,7 +1,5 @@
 import { REQUEST_CONTACT_DATA } from './constants';
 
-// This action doesn't currently have a corresponding reducer,
-// because the request shouldn't update state.
 export function requestContactData() {
   return {
     type: REQUEST_CONTACT_DATA,
