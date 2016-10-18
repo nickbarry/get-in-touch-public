633812eddebd1ce99044745b5551ef9ec546ba4f
Nick Barry <niko.barry@gmail.com>
2016-10-18 10:28:51 -0700

    chore: Cleanup per styleguide

--- server/routes.js
+++ server/routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const contacts = require('./controllers/contacts');
 
-const router = express.Router();
+const router = express.Router(); // eslint-disable-line new-cap
 
 router.get('/api/contacts', contacts.fetch);
 router.delete('/api/contact/:userId', contacts.delete);
