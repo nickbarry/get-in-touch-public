c7dade0d3aa7abb5d8ea0bc9d68e563277746a0b
Nick Barry <niko.barry@gmail.com>
2016-10-19 22:46:37 -0700

    style: Allow user to how/hide Compose pane with new button
    The last commit made a lot of improvements to the layout/appearance of the ContactCard component, but didn't actually add much functionality. Now, the button I created actually works to show/hide the Compose pane.
    
    There are also a number of other style/layout improvements to ContactCard in this commit.

--- app/containers/ContactCard/index.js
+++ app/containers/ContactCard/index.js
@@ -4,7 +4,7 @@ import { bindActionCreators } from 'redux';
 import styles from './styles.css';
 import avatar from './default-avatar.png';
 import { List, Map } from 'immutable';
-import { Panel, ListGroup, ListGroupItem, Button, Glyphicon } from 'react-bootstrap';
+import { Panel, Button, Glyphicon } from 'react-bootstrap';
 import { requestContactDeletion } from './actions';
 
 function groupStoriesByTopic(stories) {
@@ -49,9 +49,42 @@ function createOptionGroups(storiesGroupedByTopic) {
 }
 
 export class ContactCard extends React.Component { // eslint-disable-line react/prefer-stateless-function
+  constructor(props) {
+    super(props);
+    this.state = {
+      expanded: false,
+    };
+  }
+
+  onClickContactNowToggleBar(e) {
+    e.preventDefault(); // prevent normal link behavior and event bubbling
+    this.setState({ expanded: !this.state.expanded });
+  }
+
+  renderComposePane() {
+    const storiesGroupedByTopic = groupStoriesByTopic(this.props.stories);
+
+    return (
+      <div className={styles.notesAndCompose}>
+        <div className={styles.notes}>
+          <h4>Notes about {this.props.contact.get('name')}</h4>
+          <textarea name="contact--notes--edit" rows="10" defaultValue={this.props.contact.get('notes')} />
+        </div>
+        <div className={styles.compose}>
+          <h4>Compose a message</h4>
+          <select>
+            <option value="">-- share a story --</option>
+            {createOptionGroups(storiesGroupedByTopic)}
+          </select><br />
+          <textarea rows="10" />
+        </div>
+      </div>
+    );
+  }
+
   renderOptionButtons() {
     return (
-      <div className={styles.buttonGroup}>
+      <div className={`${styles.buttonGroup} pull-right`}>
         <Button className={styles.btnEdit}>
           <Glyphicon glyph="pencil" /> Edit
         </Button>
@@ -66,50 +99,42 @@ export class ContactCard extends React.Component { // eslint-disable-line react/
   }
 
   render() {
-    const storiesGroupedByTopic = groupStoriesByTopic(this.props.stories);
+    const contactFrequency = +this.props.contact.get('contactFrequency');
+    const numberOfDaysLabel = contactFrequency === 1 ?
+      'day' :
+      `${contactFrequency} days`;
 
     return (
       <Panel className={styles.contactCard}>
-        {this.renderOptionButtons()}
         <div className="container-fluid">
           <div className="row">
             <div className={`${styles.avatarColumn} col-sm-2`}>
               <img className={`${styles.avatar}`} src={avatar} alt="user avatar" />
             </div>
-            <div className="col-sm-10">
+            <div className={`${styles.contactInfo} col-sm-10`}>
+              {this.renderOptionButtons()}
               <h3 className={styles.contactName}>{this.props.contact.get('name')}</h3>
               <p className={styles.stats}>
-                Contacted { this.props.contact.get('lastContacted').format('MMM D, YYYY') }.
-                Contact every { this.props.contact.get('contactFrequency') } days
+                Contacted { this.props.contact.get('lastContacted').format('MMM D, YYYY') }<br />
+                Contact every { numberOfDaysLabel }<br />
+                <Button className={styles.justContacted} bsSize="small">
+                  <Glyphicon glyph="ok" /> Just contacted!
+                </Button>
               </p>
-              <label className={styles.composeLabel} htmlFor={`notes-chk-${this.props.contact.get('id')}`}>Message {this.props.contact.name}</label>
-              <input type="checkbox" id={`notes-chk-${this.props.contact.get('id')}`} />
-              <div className={styles.notesAndCompose}>
-                <div className={styles.notes}>
-                  <h4>Notes</h4>
-                  <textarea name="contact--notes--edit" rows="10" defaultValue={this.props.contact.get('notes')} />
-                </div>
-                <div className={styles.compose}>
-                  <h4>Compose a message</h4>
-                  <select>
-                    <option value="">-- share a story --</option>
-                    {createOptionGroups(storiesGroupedByTopic)}
-                  </select><br />
-                  <textarea rows="10" />
-                </div>
-              </div>
             </div>
           </div>
         </div>
 
-        <ListGroup fill>
-          <ListGroupItem className={`${styles.contactNowToggleBar} text-center`}>
-            Contact Now <Glyphicon glyph="chevron-down" />
-            <Button className={`${styles.btnContacted} pull-right`}>
-              <Glyphicon glyph="ok" /> Just contacted!
-            </Button>
-          </ListGroupItem>
-        </ListGroup>
+        { /* todo: make this a link, and give a on-hover color change */}
+        <div className={this.state.expanded ? styles.contactNowToggleBarExpanded : styles.contactNowToggleBar}>
+          <Button onClick={(e) => this.onClickContactNowToggleBar(e)} className={styles.contactNowToggleButton} bsSize="small">
+            {this.state.expanded ? 'Hide ' : 'Contact Now '}
+            {this.state.expanded ? <Glyphicon glyph="chevron-up" /> : <Glyphicon glyph="chevron-down" />}
+          </Button>
+        </div>
+
+        {this.state.expanded && this.renderComposePane()}
+
       </Panel>
     );
   }
