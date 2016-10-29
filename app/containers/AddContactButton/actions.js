85b02cdcda54fbc0828931d5367954917c8f74c0
Nick Barry <niko.barry@gmail.com>
2016-10-29 16:56:05 -0700

    feat: Add userId column to contacts table
    The table now has a userId column, so we can set up a one-to-many relationship between users and contacts. When requesting to create a new contact, our frontend also hard codes in Nico's userId (0). Once the redux state records that info, we'll need to stop hard coding it.

--- app/containers/AddContactButton/actions.js
+++ app/containers/AddContactButton/actions.js
@@ -2,6 +2,10 @@ import { REQUEST_ADD_CONTACT } from './constants';
 
 export function requestAddContact(newContactValues) {
   const jsContact = newContactValues.toJS();
+
+  // Hard code in Nico's userId. Soon we'll need to detect the correct userId and use that
+  jsContact.userId = 0;
+
   return {
     type: REQUEST_ADD_CONTACT,
     values: jsContact,
