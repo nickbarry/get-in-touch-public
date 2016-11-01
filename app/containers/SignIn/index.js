4cf6893aa6916bdcc8dac548872bd223c23102c9
Nick Barry <niko.barry@gmail.com>
2016-11-01 14:17:20 -0700

    feat: Add scaffolding for SignIn container
    At first, this container will just allow us to fake being signed in as various users - it will initially just be a dropdown to choose which user you want to pretend you're signed in as.

---
+++ app/containers/SignIn/index.js
@@ -0,0 +1,30 @@
+/*
+ *
+ * SignIn
+ *
+ */
+
+import React from 'react';
+import { connect } from 'react-redux';
+import styles from './styles.css';
+
+export class SignIn extends React.Component { // eslint-disable-line react/prefer-stateless-function
+  render() {
+    return (
+      <div className={styles.signIn}>
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+    return { state: state };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    dispatch,
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(SignIn);
