ce47ff8c31379d0fab18fe041c766c91beb9e498
Nick Barry <niko.barry@gmail.com>
2016-11-01 17:12:23 -0700

    feat: Ensure frontend sends correct userId to backend, pt. 2
    In the previous commit, I hadn't quite finished the following: (repeat of the previous commit's message) The frontend needs to send the correct userId to the backend, so the backend can apply the requested CRUD operation to the appropriate contacts. The backend is not yet set up to handle this information. Once we have proper authentication, we won't be sending a userId; we'll be sending a jwt or something similar.
    
    This commit should complete the frontend portion of the changes.

--- app/APIs/index.js
+++ app/APIs/index.js
@@ -1,16 +1,17 @@
 import axios from 'axios';
 
 const server = {
-  fetchAllContacts() {
-    return axios.get('/api/contacts');
+  fetchAllContacts(userId) { // todo: update backend to expect new userId data
+    console.log('userId from APIs: ', userId);
+    return axios.get(`/api/contacts?userId=${userId}`);
   },
-  updateContact(contactId, values) {
-    return axios.post(`/api/contact/${contactId}`, values);
+  updateContact(contactId, userId, values) { // todo: update backend to expect new userId data
+    return axios.post(`/api/contact/${contactId}`, { userId, values });
   },
-  deleteContact(contactId) {
-    return axios.delete(`/api/contact/${contactId}`);
+  deleteContact(contactId, userId) { // todo: update backend to expect new userId data
+    return axios.delete(`/api/contact/${contactId}?userId=${userId}`);
   },
-  addContact(newContactValues) {
+  addContact(newContactValues) { // todo: update backend to expect new userId data
     return axios.post('/api/contact', newContactValues);
   },
 };
--- app/containers/Dashboard/actions.js
+++ app/containers/Dashboard/actions.js
@@ -1,7 +1,8 @@
 import { REQUEST_CONTACT_DATA } from '../ContactCard/constants';
 
-export function requestContactData() {
+export function requestContactData(userId) {
   return {
     type: REQUEST_CONTACT_DATA,
+    userId,
   };
 }
