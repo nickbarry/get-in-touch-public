90b9fad89b43499163851a3e70e0dcb048ebd343
Nick Barry <niko.barry@gmail.com>
2016-11-04 13:19:49 -0700

    feat: Extract form styles into their own stylesheet
    Our form styles will be used across multiple forms, so they should be part of a shared css sheet.

--- app/containers/Contacts/AddContactButton/AddContactForm.js
+++ app/containers/Contacts/AddContactButton/AddContactForm.js
@@ -2,6 +2,21 @@ import React from 'react';
 import { Field, reduxForm } from '../../../../node_modules/redux-form/immutable';
 import { Button, Row } from 'react-bootstrap';
 import styles from './styles.css';
+import formStyles from '../../../assets/formStyles.css';
+
+const renderField = ({ input, name, label, type, meta: { touched, error, warning } }) => ( // eslint-disable-line react/prop-types
+  <div className={"form-group"}>
+    <label htmlFor={name} className="col-sm-3 control-label">{label}</label>
+    <div className="col-sm-9">
+      <input className={`form-control${(touched && error) ? ` ${styles.formFieldError}` : ''}`} {...input} placeholder={label} type={type} />
+      {
+        touched &&
+        ((error && <div className={styles.formErrorMessage}>{error}</div>) ||
+        (warning && <div className={styles.formWarningMessage}>{warning}</div>))
+      }
+    </div>
+  </div>
+);
 
 const AddContactForm = (props) => {
   const { handleSubmit } = props;
