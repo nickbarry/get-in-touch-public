2f699a619a528919448321daf73773aa87c0482b
Nick Barry <niko.barry@gmail.com>
2016-11-04 13:19:49 -0700

    feat: Extract form styles into their own stylesheet
    Our form styles will be used across multiple forms, so they should be part of a shared css sheet.

--- app/containers/Contacts/ContactCard/EditContact.js
+++ app/containers/Contacts/ContactCard/EditContact.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Field, reduxForm } from '../../../../node_modules/redux-form/immutable';
 import { Button, Glyphicon } from 'react-bootstrap';
 import styles from './styles.css';
+import formStyles from '../../../assets/formStyles.css';
 import {
   contactName, contactNameWarning,
   contactEmail,
@@ -27,11 +28,11 @@ const renderField = ({ input, name, label, type, meta: { touched, error, warning
   <div className={"form-group"}>
     <label htmlFor={name} className="col-sm-3 control-label">{label}</label>
     <div className="col-sm-9">
-      <input className={`form-control${(touched && error) ? ` ${styles.formFieldError}` : ''}`} {...input} placeholder={label} type={type} />
+      <input className={`form-control${(touched && error) ? ` ${formStyles.formFieldError}` : ''}`} {...input} placeholder={label} type={type} />
       {
         touched &&
-        ((error && <div className={styles.formErrorMessage}>{error}</div>) ||
-          (warning && <div className={styles.formWarningMessage}>{warning}</div>))
+        ((error && <div className={formStyles.formErrorMessage}>{error}</div>) ||
+          (warning && <div className={formStyles.formWarningMessage}>{warning}</div>))
       }
     </div>
   </div>
