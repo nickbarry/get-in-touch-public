538226511b4ac00d34eab84de67a599f71e95c90
Nick Barry <niko.barry@gmail.com>
2016-11-06 13:07:10 -0800

    feat: Use the same form for Add and Edit contact functions
    We did have two separate forms, with nearly identical structure and functionality. I anticipated adding further functionality and sophistication to both forms, and didn't relish the thought of making matching changes on both forms. So now both functions use the same form component, and pass in different props.

---
+++ app/containers/Contacts/AddEditContactForm/tests/index.test.js
@@ -0,0 +1,11 @@
+// import AddEditContactForm from '../index';
+
+import expect from 'expect';
+// import { shallow } from 'enzyme';
+// import React from 'react';
+
+describe('<AddEditContactForm />', () => {
+  it('Expect to have unit tests specified', () => {
+    expect(true).toEqual(false);
+  });
+});
